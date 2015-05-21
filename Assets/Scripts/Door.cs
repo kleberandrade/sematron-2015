c0beef1d29653b448277987df3ee01b4f8d0b547
Kleber de Oliveira Andrade <pdjkleber@gmail.com>
2015-05-21 16:29:05 -0300

    Código que encapsula a animação da porta

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+    private Animator myAnimator;
+
+    void Awake()
+    {
+        myAnimator = GetComponent<Animator>();
+    }
+
+    public void Open()
+    {
+        myAnimator.SetTrigger("Open");
+    }
+
+    public void Close()
+    {
+        myAnimator.SetTrigger("Close");
+    }
+
+}
