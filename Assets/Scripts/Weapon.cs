4ebf7383b188ee5099ae867ba034c389b115201e
Kleber de Oliveira Andrade <pdjkleber@gmail.com>
2015-05-21 17:23:11 -0300

    Script da arma

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon : MonoBehaviour 
+{
+    public float force = 10.0f;
+    public Rigidbody bulletPrefab;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Rigidbody go = (Rigidbody) Instantiate(bulletPrefab);
+            go.position = transform.position;
+            go.rotation = transform.rotation;
+            go.AddForce(transform.forward * force);
+        }
+    }
+}
