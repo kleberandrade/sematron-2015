e73448a50711d8e285746f9768ea52d7aca8f257
Kleber de Oliveira Andrade <pdjkleber@gmail.com>
2015-05-21 21:16:27 -0300

    Atirando bullets com a weapon

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -3,16 +3,16 @@ using System.Collections;
 
 public class Weapon : MonoBehaviour 
 {
-    public float force = 10.0f;
+    public float force = 1000.0f;
     public Rigidbody bulletPrefab;
 
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
         {
-            Rigidbody go = (Rigidbody) Instantiate(bulletPrefab);
-            go.position = transform.position;
-            go.rotation = transform.rotation;
+            Rigidbody go = (Rigidbody)Instantiate(bulletPrefab,
+                transform.position, transform.rotation);
+
             go.AddForce(transform.forward * force);
         }
     }
